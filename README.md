10a8889dc4da1d628af1953c3fafe62b094fe023
Sumit Ranjan <34721357+sumitsarraf@users.noreply.github.com>
2022-09-27 11:18:18 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # idle-timeout-angular
 How to Implement Idle Timeout in Angular
+
+# Here are the libraries used for this project.
+
+ngx-bootstrap for the UI
+ng-idle for the idle timeout
+
+dependencies in the existing application.
+
+npm install --save @ng-idle/core @ng-idle/keepalive angular2-moment
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive'; // this includes the core NgIdleModule but includes keepalive providers for easy wireup
+import { MomentModule } from 'angular2-moment'; // optional, provides moment-style pipes for date formatting
+
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgIdleKeepaliveModule.forRoot(),
+    MomentModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
